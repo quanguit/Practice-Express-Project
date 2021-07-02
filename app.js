34b4f137204afe1a764a59ca4771d60d597cd97b
quanguit <nhatquang25012000@gmail.com>
2021-07-02 23:23:11 +0700

    add bootstrap v5.0

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const path = require("path");
 const mongoose = require("mongoose");
-const Campground = require("./models/campground");
+const ejsMate = require("ejs-mate");
 const methodOverride = require("method-override");
+const Campground = require("./models/campground");
 
 mongoose.connect("mongodb://localhost:27017/yelp-camp", {
   useNewUrlParser: true,
@@ -18,6 +19,7 @@ db.once("open", () => {
 
 const app = express();
 
+app.engine("ejs", ejsMate);
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 app.use(express.urlencoded({ extended: true }));
@@ -61,7 +63,9 @@ app.get("/campgrounds/:id/edit", async (req, res) => {
 
 app.put("/campgrounds/:id", async (req, res) => {
   const { id } = req.params;
-  const campground = await Campground.findByIdAndUpdate(id, {...req.body.campground});
+  const campground = await Campground.findByIdAndUpdate(id, {
+    ...req.body.campground,
+  });
   res.redirect(`/campgrounds/${campground._id}`);
 });
 
--- seeds/index.js
+++ seeds/index.js
@@ -23,9 +23,14 @@ const seedDb = async () => {
   for (let i = 0; i < 50; i++) {
     // const random1000 = Math.floor(Math.random() * 1000); => cities[random1000].city
     const random1000 = sample(cities);
+    const price = Math.floor(Math.random() * 20) + 10;
     const camp = new Campground({
       location: `${random1000.city}, ${random1000.state}`,
       title: `${sample(descriptors)} ${sample(places)}`,
+      image: "https://source.unsplash.com/collection/483251/",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, amet? Sit cum, soluta doloribus sapiente voluptatem facere hic impedit architecto ipsam perferendis ipsum consequuntur, optio tempore mollitia dicta nihil aliquid!",
+      price,
     });
     await camp.save();
   }
